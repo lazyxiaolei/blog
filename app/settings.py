26362481279487f59d26096e69f22ae51bfaa165
lazyxiaolei <366849574@qq.com>
2020-12-27 21:33:10 +0800

    V1.0

--- app/settings.py
+++ app/settings.py
@@ -4,7 +4,7 @@
 @FilePath: /blog/app/settings.py
 @Author: liu.junlei@foxmail.com
 @Date: 2020-12-06 17:50:39
-@LastEditTime: 2020-12-26 22:42:55
+@LastEditTime: 2020-12-27 21:32:36
 """
 
 MYSQL = {
@@ -15,19 +15,16 @@ MYSQL = {
     'database': 'blog',
 }
 
-
 REDIS = {
     'host': 'localhost',
     'port': 6379,
     'db': 0,
 }
 
-
 EMAIL = {
-    'sender': 'liu.junlei@foxmail.com',
-    'password': 'aixwpypxxuuxbgfe',
+    'sender': '',  # 发送邮箱
+    'password': '',  # 邮箱认证密码
 }
-
 
 UPLOAD_FACE_PATH = '/root/blog/static/upload_face'
 ALLOWED_EXTENSIONS = set(['jpg', 'png', 'jpeg'])
